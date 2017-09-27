06c4fe95a1e2103fb7d844dfe7be2a7c0cac10b7
liangping <18786721@qq.com>
2017-09-27 19:58:23 +0800

    完成阿里云短信发送对接

---
+++ aliyun-mns-spring-boot-starter/src/main/java/io/yaoling/starter/aliyun/mns/AliyunMessageSenderAutoConfiguration.java
@@ -0,0 +1,27 @@
+package io.yaoling.starter.aliyun.mns;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by liangping on 2017/9/27 0027.
+ * <p>
+ * 江苏摇铃网络科技有限公司，版权所有。
+ * Copyright (C) 2015-2017 All Rights Reserved.
+ */
+@Configuration
+@EnableConfigurationProperties(AliyunMessageSenderPropertiesConfig.class)//指定类的配置
+@ConditionalOnClass(AliyunMessageSender.class)
+@ConditionalOnProperty(prefix = "spring.aliyun.oss",value = "enable",matchIfMissing = true)//指定的属性是否有指定的值
+public class AliyunMessageSenderAutoConfiguration {
+    @Bean
+    @ConditionalOnMissingBean(AliyunMessageSender.class)//当容器中没有指定Bean的情况下
+    public AliyunMessageSender aliyunMessageSender() {
+        return new AliyunMessageSender();
+    }
+}
+
---
+++ aliyun-mns-spring-boot-starter/src/main/java/io/yaoling/starter/aliyun/mns/AliyunMessageSenderPropertiesConfig.java
@@ -0,0 +1,67 @@
+package io.yaoling.starter.aliyun.mns;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Created by liangping on 2017/9/27 0027.
+ * <p>
+ * 江苏摇铃网络科技有限公司，版权所有。
+ * Copyright (C) 2015-2017 All Rights Reserved.
+ */
+@ConfigurationProperties(prefix = "spring.aliyun.mns")
+public class AliyunMessageSenderPropertiesConfig {
+    private boolean enable = false;
+    private String endpoint="";
+    private String accessKeyId="";
+    private String accessKeySecret="";
+    private String topic="";
+    private String signature;
+
+    public boolean isEnable() {
+        return enable;
+    }
+
+    public void setEnable(boolean enable) {
+        this.enable = enable;
+    }
+
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
+
+    public String getAccessKeyId() {
+        return accessKeyId;
+    }
+
+    public void setAccessKeyId(String accessKeyId) {
+        this.accessKeyId = accessKeyId;
+    }
+
+    public String getAccessKeySecret() {
+        return accessKeySecret;
+    }
+
+    public void setAccessKeySecret(String accessKeySecret) {
+        this.accessKeySecret = accessKeySecret;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+}
