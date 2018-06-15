65953220b59bb8754a1cc2cdc4be19dbef268b4c
liangping <18786721@qq.com>
2018-06-15 12:41:48 +0800

    完成消息监听功能

--- weixin-spring-boot-starter/src/main/java/io/yaoling/starter/weixin/WeixinAutoConfiguration.java
+++ weixin-spring-boot-starter/src/main/java/io/yaoling/starter/weixin/WeixinAutoConfiguration.java
@@ -1,18 +1,14 @@
 package io.yaoling.starter.weixin;
 
 import io.yaoling.starter.weixin.component.api.WeixinComponentApi;
-import io.yaoling.starter.weixin.listener.AbstractWeixinPayNotifyHandler;
-import io.yaoling.starter.weixin.listener.MessageListenerController;
-import io.yaoling.starter.weixin.listener.MessageListenerLocator;
-import io.yaoling.starter.weixin.listener.PayNotifyListenerController;
+import io.yaoling.starter.weixin.listener.*;
 import io.yaoling.starter.weixin.mp.WeixinApi;
 import io.yaoling.starter.weixin.mp.WeixinPayApi;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -22,33 +18,27 @@ import org.springframework.context.annotation.Configuration;
  * Copyright (C) 2015-2017 All Rights Reserved.
  */
 @Configuration
-@EnableConfigurationProperties(WeixinPropertiesConfig.class)//指定类的配置
-@ConditionalOnClass(WeixinApi.class)
-@ConditionalOnProperty(prefix = "yaoling.starter.weixin",value = "enable",matchIfMissing = true)//指定的属性是否有指定的值
+@EnableConfigurationProperties(WeixinPropertiesConfig.class)
+@ConditionalOnProperty(prefix = "yaoling.starter.weixin", value = "enable",havingValue = "true",matchIfMissing = true)//指定的属性是否有指定的值
+@ComponentScan("io.yaoling.starter.weixin.listener")
 public class WeixinAutoConfiguration {
 
     @Bean
-    @ConditionalOnMissingBean(WeixinApi.class)//当容器中没有指定Bean的情况下
+    @ConditionalOnMissingBean(WeixinApi.class)
     public WeixinApi weixinApi(){
         return new WeixinApi();
     }
 
     @Bean
-    @ConditionalOnMissingBean(WeixinPayApi.class)//当容器中没有指定Bean的情况下
+    @ConditionalOnMissingBean(WeixinPayApi.class)
     public WeixinPayApi weixinPayApi(){
         return new WeixinPayApi();
     }
 
     @Bean
-    @ConditionalOnMissingBean(WeixinComponentApi.class)//当容器中没有指定Bean的情况下
+    @ConditionalOnMissingBean(WeixinComponentApi.class)
     public WeixinComponentApi weixinComponentApi(){
         return new WeixinComponentApi();
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(MessageListenerController.class)
-    public MessageListenerController messageListenerController(){
-        return new MessageListenerController();
     }
 
     @Bean
@@ -58,8 +48,9 @@ public class WeixinAutoConfiguration {
     }
 
     @Bean
-    @ConditionalOnBean(AbstractWeixinPayNotifyHandler.class)
-    public PayNotifyListenerController payNotifyListenerController(){
-        return new PayNotifyListenerController();
+    @ConditionalOnMissingBean(MessageListener.class)
+    public ConfigSuccessMessageListener configSuccessMessageListener(){
+        return new ConfigSuccessMessageListener();
     }
+
 }
---
+++ weixin-spring-boot-starter/src/main/java/io/yaoling/starter/weixin/listener/ConfigSuccessMessageListener.java
@@ -0,0 +1,21 @@
+package io.yaoling.starter.weixin.listener;
+
+import io.yaoling.starter.weixin.message.EventMessage;
+import io.yaoling.starter.weixin.message.Message;
+import io.yaoling.starter.weixin.message.MessageFactory;
+import io.yaoling.starter.weixin.message.TextMessage;
+
+public class ConfigSuccessMessageListener extends AbstractEventMessageListener {
+
+    @Override
+    public Message onEventMessageReceived(EventMessage message, MessageContext setting) {
+        TextMessage reply = MessageFactory.createTextMessage(message);
+        reply.setContent("接口已经配置完成，请继承AbstractMessageListener实现你自己的消息监听");
+        return reply;
+    }
+
+    @Override
+    public boolean trigger(Message message) {
+        return true;
+    }
+}
