6413875095e972e08947c0feb461227b7d19b878
liangping <18786721@qq.com>
2018-07-15 12:36:35 +0800

    优化接口字段

--- yaoling-spring-boot-starter-core/src/main/java/io/yaoling/third/pay/PayTransaction.java
+++ yaoling-spring-boot-starter-core/src/main/java/io/yaoling/third/pay/PayTransaction.java
@@ -1,74 +1,45 @@
 package io.yaoling.third.pay;
 
-import java.util.Date;
-
 /**
  * 支付申请信息。
+ * 注意：同一个订单可能发起多次支付。
  * @author ping
  *
  */
 public interface PayTransaction extends PayStatusConstant{
 
 	/**
-	 * 所以应的订单ID
+	 * 支付通知处理handler的名字。
+	 * 指定这个名字的handler来处里本条支付的通知成功处理。
 	 * @return
 	 */
-	public String getOrderId();
-	/**
-	 * 支付单ID
-	 * @return
-	 */
-	public String getId();
+	String handlerName();
 
 	/**
-	 * 外部应用的绑定授权appid. 例如：微信的appid
-	 * @return
+	 * 所以应的订单ID
+	 * @return orderId
 	 */
-	public String getAppid();
+	String getOrderId();
+
 	/**
-	 * 发起操作人
-	 * @return
+	 * 支付单ID
+	 * @return id
 	 */
-	public String getOp();
-	/**
-	 * 支付流水号
-	 * @return
-	 */
-	public String getPayNo();
-	/**
-	 * 支付方式
-	 * @return
-	 */
-	public Payment getPayment();
+	String getId();
 	/**
 	 * 支付金额，单位（分）
-	 * @return
+	 * @return amount
 	 */
-	public int getAmount();
-	/**
-	 * 外部商户号
-	 * @return
-	 */
-	public String getMchId();
+	int getAmount();
 	/**
 	 * 外部用户ID
-	 * @return
+	 * @return openid
 	 */
-	public String getOpenid();
-	/**
-	 * 发起支付日期
-	 * @return
-	 */
-	public Date getCreateTime();
-	/**
-	 * 支付完成时间
-	 * @return
-	 */
-	public Date getPayTime();
+	String getOpenid();
 	/**
 	 * 发起支付的地址
-	 * @return
+	 * @return ip
 	 */
-	public String getIpAddress();
+	String getIpAddress();
 
 }
